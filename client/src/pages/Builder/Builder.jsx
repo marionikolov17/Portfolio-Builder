78b0447340b90e02b4d3040487278171a92ae2d9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-31 23:23:50 +0300

    Import top bar component in builder page

--- client/src/pages/Builder/Builder.jsx
+++ client/src/pages/Builder/Builder.jsx
@@ -1,6 +1,11 @@
+import TopBar from "../../shared/components/TopBar/TopBar";
+
 export default function Builder() {
     return (
         <>
+            <section className="grow min-h-full">
+                <TopBar />
+            </section>
         </>
     )
 }
