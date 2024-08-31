9e9d7be94f959192b90a13dac57ef9a4b494a1e6
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-31 12:44:23 +0300

    Update Layout.jsx

--- client/src/layouts/Layout/Layout.jsx
+++ client/src/layouts/Layout/Layout.jsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 export default function Layout() {
     return (
-        <></>
+        <>
+            <main className="w-full min-h-full overflow-x-hidden absolute font-montserrat flex">
+                <Outlet />
+            </main>
+        </>
     )
 }
