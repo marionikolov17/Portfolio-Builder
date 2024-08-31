5b58049dd644eb7c984d30ff851ddcfb3189868a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-31 12:45:55 +0300

    Import Desktop navigation in layout

--- client/src/layouts/Layout/Layout.jsx
+++ client/src/layouts/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import DesktopNavigation from "../../features/layout/DesktopNavigation/DesktopNavigation";
 
 export default function Layout() {
     return (
         <>
             <main className="w-full min-h-full overflow-x-hidden absolute font-montserrat flex">
+                <DesktopNavigation />
                 <Outlet />
             </main>
         </>
