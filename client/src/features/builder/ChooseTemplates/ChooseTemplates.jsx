d431c8d387bfdb0a28927dc536cbd7d08b8949cb
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-01 13:10:17 +0300

    Create a template card

--- client/src/features/builder/ChooseTemplates/ChooseTemplates.jsx
+++ client/src/features/builder/ChooseTemplates/ChooseTemplates.jsx
@@ -1,11 +1,12 @@
 import { IoSearchOutline } from "react-icons/io5";
+import TemplateCard from "../TemplateCard/TemplateCard";
 
 export default function ChooseTemplates() {
   return (
     <>
       <div className="w-full px-10">
         <h1 className="mt-8 text-3xl font-bold">Choose a Template</h1>
-        <form className="mt-4">
+        <form className="mt-6">
             <div className="flex items-center relative bg-white max-w-lg rounded-lg shadow overflow-hidden">
                 <input type="text" placeholder="Search templates..." className="ps-4 outline-none py-2 grow shrink-0" required />
                 <button className="py-2 flex items-center shrink-0 px-4">
@@ -13,6 +14,9 @@ export default function ChooseTemplates() {
                 </button>
             </div>
         </form>
+        <div className="mt-8 grow flex-wrap flex gap-8">
+          <TemplateCard />
+        </div>
       </div>
     </>
   );
--- client/src/features/builder/TemplateCard/TemplateCard.jsx
+++ client/src/features/builder/TemplateCard/TemplateCard.jsx
@@ -1,5 +1,37 @@
+import { Link } from "react-router-dom";
+
 export default function TemplateCard() {
     return (
-        <></>
+        <>
+            <div className="w-96 h-96 overflow-hidden rounded-lg">
+                {/* Image */}
+                <div className="w-full h-56 flex justify-center overflow-hidden">
+                    <img src="/images/test1.png" className="object-cover" alt="template image" />
+                </div>
+                {/* Naming */}
+                <div className="mt-3 flex items-center px-2">
+                    <div className="flex justify-start grow shrink-0">
+                        <h2 className="text-2xl font-bold">Standart</h2>
+                    </div>
+                    <div className="flex justify-end grow shrink-0">
+                        <Link className="border border-gray-700 rounded-lg font-bold text-sm flex justify-center items-center py-1.5 px-4 shadow-sm bg-white hover:bg-gray-100">
+                            Live Demo
+                            <div className="w-2 h-2 ms-2 bg-strongRed rounded-full flicker-animation"></div>
+                        </Link>
+                    </div>
+                </div>
+                {/* Tags */}
+                <div className="flex mt-3 px-2">
+                    <p className="bg-primaryBlue rounded-full py-1.5 px-4 text-white me-2 text-sm shadow">#Web</p>
+                    <p className="bg-primaryBlue rounded-full py-1.5 px-4 text-white me-2 text-sm shadow">#Development</p>
+                </div>
+                {/* Choose button */}
+                <div className="px-2 mt-6">
+                    <button className="w-full flex items-center justify-center py-2 rounded-lg shadow-sm bg-gray-100 text-gray-900 font-medium text-sm hover:bg-primaryBlue hover:text-white transition duration-300">
+                        Choose
+                    </button>
+                </div>
+            </div>
+        </>
     )
 }
