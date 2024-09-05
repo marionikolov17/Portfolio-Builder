d73a8ef4e120efab72fe7d9e777c8730c03b1070
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-05 17:51:23 +0300

    Update StandartTheme.jsx

--- client/src/themes/standart/StandartTheme.jsx
+++ client/src/themes/standart/StandartTheme.jsx
@@ -1,3 +1,4 @@
+import AboutSectionStandart from "../../features/themes/standart/components/AboutSectionStandart";
 import HeroSectionStandart from "../../features/themes/standart/components/HeroSectionStandart";
 
 export default function StandartTheme() {
@@ -6,6 +7,7 @@ export default function StandartTheme() {
       <div className="w-full min-h-full px-2 sm:px-10 pb-2 overflow-x-hidden overflow-y-scroll no-scrollbar">
         <h1 className="mt-8 text-3xl font-bold">Edit your portfolio</h1>
         <HeroSectionStandart />
+        <AboutSectionStandart />
       </div>
     </>
   );
