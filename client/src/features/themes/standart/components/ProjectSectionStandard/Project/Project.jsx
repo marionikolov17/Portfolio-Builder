4f8e9f4f312e952181a699c4e34fda0bb91c49b9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-06 12:35:05 +0300

    Create project card header info

--- client/src/features/themes/standart/components/ProjectSectionStandard/Project/Project.jsx
+++ client/src/features/themes/standart/components/ProjectSectionStandard/Project/Project.jsx
@@ -1,5 +1,30 @@
+import { CiEdit } from "react-icons/ci";
+import { MdOutlineDelete, MdOutlineDragIndicator } from "react-icons/md";
+
 export default function Project() {
     return (
-        <></>
+        <>
+            <div className="mt-8 w-full sm:w-[70%] min-h-14 bg-white rounded-lg shadow overflow-hidden">
+                {/* Header info and action buttons */}
+                <div className="h-14 flex items-center">
+                    {/* Image and Title */}
+                    <div className="grow flex justify-start items-center h-full">
+                        <div className="h-full w-20 flex">
+                            <img 
+                                src="https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg" 
+                                className="object-cover" 
+                                alt="project image" />
+                        </div>
+                        <h4 className="ms-2 font-bold">Fitness-Manager</h4>
+                    </div>
+                    {/* Buttons */}
+                    <div className="grow flex justify-end items-center">
+                        <CiEdit className="text-3xl mx-2 cursor-pointer text-primaryBlue"/>
+                        <MdOutlineDelete className="text-3xl mx-2 cursor-pointer text-strongRed"/>
+                        <MdOutlineDragIndicator className="text-3xl mx-2 cursor-pointer text-slate-400"/>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
--- client/src/features/themes/standart/components/ProjectSectionStandard/ProjectSectionStandard.jsx
+++ client/src/features/themes/standart/components/ProjectSectionStandard/ProjectSectionStandard.jsx
@@ -1,5 +1,35 @@
+import { MdAssignmentAdd } from "react-icons/md";
+import Project from "./Project/Project";
+
 export default function ProjectSectionStandard() {
-    return (
-        <></>
-    )
+  return (
+    <>
+      {/* Project Section Header */}
+      <div className="w-full flex items-center bg-white rounded-lg mt-8 shadow-sm z-20">
+        <div className="grow flex justify-start px-4 py-3">
+          <h2 className="sm:text-xl font-bold">Projects Section</h2>
+        </div>
+        <div className="grow flex justify-end px-4 py-3">
+          <label className="inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" checked />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+            <span className="ms-3 text-sm font-medium text-gray-900">
+              Visible
+            </span>
+          </label>
+        </div>
+      </div>
+      {/* Add project button */}
+      <button className="mt-8 flex items-center py-2 px-4 rounded-lg shadow bg-white hover:bg-gray-100 hover:text-primaryBlue">
+        <MdAssignmentAdd className="me-2 text-xl"/>
+        Add new project
+      </button>
+      {/* Added projects section */}
+      <div className="mt-8 flex flex-col items-center">
+        <h3 className="font-bold text-xl">Projects</h3>
+        {/* Project */}
+        <Project />
+      </div>
+    </>
+  );
 }
