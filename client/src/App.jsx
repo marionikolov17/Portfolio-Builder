5bd26f89cb212efb2345076ae840bc0957a5ac48
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-09 13:06:02 +0300

    Make a route for profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom"
 import Builder from "./pages/Builder/Builder"
 import Layout from "./layouts/Layout/Layout"
+import Profile from "./pages/Profile/Profile"
 
 function App() {
   return (
     <>
       <Routes>
+        {/* Layout routes */}
         <Route path="/" element={<Layout />}>
           <Route index Component={Builder}/>
         </Route>
+        <Route path="/profile" Component={Profile}/>
       </Routes>
     </>
   )
