b21aecebe72d3760c3d79813549eb7b1518daa88
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-09 14:04:26 +0300

    Create home your templates section

--- client/src/features/home/components/YourTemplateCard/YourTemplateCard.jsx
+++ client/src/features/home/components/YourTemplateCard/YourTemplateCard.jsx
@@ -1,7 +1,15 @@
+import { CiEdit } from "react-icons/ci";
+import { MdDone } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 export default function YourTemplateCard() {
   return (
     <>
-      <div className="w-full sm:w-96 h-96 mb-4 overflow-hidden rounded-lg">
+      <div className="w-full sm:w-96 h-96 mb-4 overflow-hidden rounded-lg relative">
+        {/* Selecting container */}
+        <div className="z-40 absolute top-0 right-0 m-4 bg-white w-6 h-6 shadow border border-primaryBlue cursor-pointer flex items-center justify-center">
+            <MdDone className="text-primaryBlue text-xl"/>
+        </div>
         {/* Image */}
         <div className="w-full h-56 flex justify-center overflow-hidden">
           <img
@@ -16,7 +24,9 @@ export default function YourTemplateCard() {
             <h2 className="text-2xl font-bold">Standart</h2>
           </div>
           <div className="flex justify-end grow shrink-0">
-            
+            <Link>
+                <CiEdit className="text-3xl cursor-pointer text-primaryBlue"/>
+            </Link>
           </div>
         </div>
         {/* Tags */}
@@ -27,12 +37,6 @@ export default function YourTemplateCard() {
           <p className="bg-primaryBlue rounded-full py-1.5 px-4 text-white me-2 text-sm shadow">
             #Development
           </p>
-        </div>
-        {/* Choose button */}
-        <div className="px-2 mt-6">
-          <button className="w-full flex items-center justify-center py-2 rounded-lg shadow-sm bg-gray-100 text-gray-900 font-medium text-sm hover:bg-primaryBlue hover:text-white transition duration-300">
-            Choose
-          </button>
         </div>
       </div>
     </>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
-import { MdDone } from "react-icons/md";
+import { MdDone, MdOutlineTipsAndUpdates } from "react-icons/md";
 import TopBar from "../../shared/components/TopBar/TopBar";
+import YourTemplateCard from "../../features/home/components/YourTemplateCard/YourTemplateCard";
 
 export default function Home() {
     return (
         <>
-            <section className="grow max-w-full min-h-full pb-10 overflow-x-hidden">
+            <section className="grow max-w-full min-h-full overflow-x-hidden">
                 <TopBar pageName={"Home"}/>
                 <div className="w-full px-3 sm:px-10">
                     {/* Portfolio link section */}
@@ -22,6 +23,16 @@ export default function Home() {
                             <MdDone className="text-xl font-bold text-white" title="Save"/>
                         </button>
                     </div>   
+                    {/* Choosing portfolio template */}
+                    <h3 className="mt-8 font-bold text-xl">Your Templates</h3>
+                    {/* Tip container */}
+                    <div className="mt-2 px-4 flex flex-wrap max-w-full sm:max-w-max mb-1 items-center py-2 border text-sm bg-primaryBlue me-2 rounded-lg text-blue-100">
+                        <MdOutlineTipsAndUpdates className="me-2 text-2xl"/>
+                        Tip: Choose which template to display on your page.
+                    </div>
+                    <div className="mt-8 flex justify-center sm:justify-start flex-wrap gap-x-4 gap-y-2">
+                        <YourTemplateCard />
+                    </div>
                 </div>
             </section>
         </>
