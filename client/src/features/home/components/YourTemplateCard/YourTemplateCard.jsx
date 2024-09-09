dd7b38ca732f2ac984118f7945f03b48f54fb6b3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-09 14:11:33 +0300

    Update YourTemplateCard.jsx

--- client/src/features/home/components/YourTemplateCard/YourTemplateCard.jsx
+++ client/src/features/home/components/YourTemplateCard/YourTemplateCard.jsx
@@ -8,7 +8,7 @@ export default function YourTemplateCard() {
       <div className="w-full sm:w-96 h-96 mb-4 overflow-hidden rounded-lg relative">
         {/* Selecting container */}
         <div className="z-40 absolute top-0 right-0 m-4 bg-white w-6 h-6 shadow border border-primaryBlue cursor-pointer flex items-center justify-center">
-            <MdDone className="text-primaryBlue text-xl"/>
+          <MdDone className="text-primaryBlue text-xl" />
         </div>
         {/* Image */}
         <div className="w-full h-56 flex justify-center overflow-hidden">
@@ -25,7 +25,7 @@ export default function YourTemplateCard() {
           </div>
           <div className="flex justify-end grow shrink-0">
             <Link>
-                <CiEdit className="text-3xl cursor-pointer text-primaryBlue"/>
+              <CiEdit className="text-3xl cursor-pointer text-primaryBlue" />
             </Link>
           </div>
         </div>
@@ -37,6 +37,12 @@ export default function YourTemplateCard() {
           <p className="bg-primaryBlue rounded-full py-1.5 px-4 text-white me-2 text-sm shadow">
             #Development
           </p>
+        </div>
+        {/* Choose button */}
+        <div className="px-2 mt-6">
+          <button className="w-full flex items-center justify-center py-2 rounded-lg shadow-sm bg-gray-100 text-gray-900 font-medium text-sm hover:bg-primaryBlue hover:text-white transition duration-300">
+            Download - <span className="ms-1 font-bold">9.99 $</span>
+          </button>
         </div>
       </div>
     </>
