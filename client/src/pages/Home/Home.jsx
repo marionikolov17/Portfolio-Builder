54b7e624da306a7676ad6ae8823553e5ba003c56
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-09 13:17:15 +0300

    Import the top bar into home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,14 @@
+import TopBar from "../../shared/components/TopBar/TopBar";
+
 export default function Home() {
     return (
-        <></>
+        <>
+            <section className="grow max-w-full min-h-full pb-10 overflow-x-hidden">
+                <TopBar pageName={"Home"}/>
+                <div className="w-full">
+
+                </div>
+            </section>
+        </>
     )
 }
