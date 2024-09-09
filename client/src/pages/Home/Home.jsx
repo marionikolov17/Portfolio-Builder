b65f730ab5903cb9f6789ffd8f7cb4b07c2a4c70
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-09 13:37:53 +0300

    Create home portfolio link input

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { MdDone } from "react-icons/md";
 import TopBar from "../../shared/components/TopBar/TopBar";
 
 export default function Home() {
@@ -5,8 +6,22 @@ export default function Home() {
         <>
             <section className="grow max-w-full min-h-full pb-10 overflow-x-hidden">
                 <TopBar pageName={"Home"}/>
-                <div className="w-full">
-
+                <div className="w-full px-3 sm:px-10">
+                    {/* Portfolio link section */}
+                    <h3 className="mt-8 font-bold text-xl">Portfolio Link</h3>
+                    <div className="mt-2 bg-white rounded-lg shadow max-w-full sm:w-max flex items-center overflow-hidden">
+                        <div className="px-4 py-2 border-e">
+                            <p className="text-sm">portfolio/</p>
+                        </div>
+                        <input 
+                            type="text"
+                            placeholder="e.g marionikolovdev" 
+                            className="outline-none px-4 py-2"
+                        />
+                        <button className="flex shrink-0 grow items-center justify-center bg-primaryBlue h-10 w-8">
+                            <MdDone className="text-xl font-bold text-white" title="Save"/>
+                        </button>
+                    </div>   
                 </div>
             </section>
         </>
