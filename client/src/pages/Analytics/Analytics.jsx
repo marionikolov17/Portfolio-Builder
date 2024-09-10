f53e16e89cec13a38288faf0ed0cb947a6b99d43
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-10 21:40:05 +0300

    Import ActivityMessage in analytics page

--- client/src/pages/Analytics/Analytics.jsx
+++ client/src/pages/Analytics/Analytics.jsx
@@ -1,3 +1,4 @@
+import ActivityMessage from "../../features/analytics/components/ActivityMessage/ActivityMessage";
 import TopBar from "../../shared/components/TopBar/TopBar";
 
 export default function Analytics() {
@@ -6,12 +7,13 @@ export default function Analytics() {
       <section className="grow max-w-full min-h-full overflow-x-hidden">
         <TopBar pageName={"Analytics"} />
         <div className="w-full px-3 sm:px-10">
+          {/* Analytics boxes */}  
           <div className="mt-8 w-full flex flex-wrap items-center lg:justify-start justify-center gap-x-4 gap-y-4">
             {/* Total Visitors */}
             <div className="w-80 h-32 bg-white rounded-lg shadow py-2 px-4 flex flex-col justify-center">
                 <h4 className="font-bold">Total Visitors</h4>
                 <h2 className="mt-2 font-bold text-4xl">11 003</h2>
-                <p className="text-sm text-slate-400">For all time</p>
+                <p className="text-sm text-slate-400">All time</p>
             </div>
             {/* New Visitors */}
             <div className="w-80 h-32 bg-white rounded-lg shadow py-2 px-4 flex flex-col justify-center">
@@ -24,6 +26,13 @@ export default function Analytics() {
                 <h4 className="font-bold">Average session</h4>
                 <h2 className="mt-2 font-bold text-4xl flex items-center">1m 49s <span className="ms-2 text-sm text-green-500">(+1m 02s)</span></h2>
                 <p className="text-sm text-slate-400">Last 7 days</p>
+            </div>
+            {/* Activity notifications */}
+            <div className="w-full mt-8">
+                <h3 className="font-bold text-xl">Activity</h3>
+                {/* <p className="mt-2">There are no activity, yet.</p> */}
+                {/* Activity messages here */}
+                <ActivityMessage />
             </div>
           </div>
         </div>
