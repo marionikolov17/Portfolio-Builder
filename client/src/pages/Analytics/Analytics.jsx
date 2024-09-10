34840ee73b4ec80354a824c69e5e8666aa9d9250
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-10 20:42:21 +0300

    Import TopBar in analytics page

--- client/src/pages/Analytics/Analytics.jsx
+++ client/src/pages/Analytics/Analytics.jsx
@@ -1,5 +1,11 @@
+import TopBar from "../../shared/components/TopBar/TopBar";
+
 export default function Analytics() {
     return (
-        <></>
+        <>
+            <section className="grow max-w-full min-h-full overflow-x-hidden">
+                <TopBar pageName={"Analytics"}/>
+            </section>
+        </>
     )
 }
