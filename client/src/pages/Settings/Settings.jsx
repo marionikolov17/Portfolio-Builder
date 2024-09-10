599e63d8339f8b879659da2eda223caf76b2c090
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-10 22:05:25 +0300

    Import Top Bar in settings page

--- client/src/pages/Settings/Settings.jsx
+++ client/src/pages/Settings/Settings.jsx
@@ -1,5 +1,14 @@
+import TopBar from "../../shared/components/TopBar/TopBar";
+
 export default function Settings() {
     return (
-        <></>
+        <>
+            <section className="grow max-w-full min-h-full overflow-x-hidden">
+                <TopBar pageName={"Settings"}/>
+                <div className="w-full px-3 sm:px-10">
+
+                </div>
+            </section>
+        </>
     )
 }
