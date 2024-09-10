8497ac0fc1de3e8542c1f6588322d67057166972
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-10 21:33:03 +0300

    Create analytics page boxes

--- client/src/pages/Analytics/Analytics.jsx
+++ client/src/pages/Analytics/Analytics.jsx
@@ -5,8 +5,27 @@ export default function Analytics() {
     <>
       <section className="grow max-w-full min-h-full overflow-x-hidden">
         <TopBar pageName={"Analytics"} />
-        <div className="w-full px-4">
-          
+        <div className="w-full px-3 sm:px-10">
+          <div className="mt-8 w-full flex flex-wrap items-center lg:justify-start justify-center gap-x-4 gap-y-4">
+            {/* Total Visitors */}
+            <div className="w-80 h-32 bg-white rounded-lg shadow py-2 px-4 flex flex-col justify-center">
+                <h4 className="font-bold">Total Visitors</h4>
+                <h2 className="mt-2 font-bold text-4xl">11 003</h2>
+                <p className="text-sm text-slate-400">For all time</p>
+            </div>
+            {/* New Visitors */}
+            <div className="w-80 h-32 bg-white rounded-lg shadow py-2 px-4 flex flex-col justify-center">
+                <h4 className="font-bold">New Visitors</h4>
+                <h2 className="mt-2 font-bold text-4xl"><span className="text-green-500 me-2">+</span>1023</h2>
+                <p className="text-sm text-slate-400">Last 7 days</p>
+            </div>
+            {/* Average Time */}
+            <div className="w-80 h-32 bg-white rounded-lg shadow py-2 px-4 flex flex-col justify-center">
+                <h4 className="font-bold">Average session</h4>
+                <h2 className="mt-2 font-bold text-4xl flex items-center">1m 49s <span className="ms-2 text-sm text-green-500">(+1m 02s)</span></h2>
+                <p className="text-sm text-slate-400">Last 7 days</p>
+            </div>
+          </div>
         </div>
       </section>
     </>
