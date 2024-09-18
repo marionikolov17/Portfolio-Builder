939286dba179993a1a23555864735c1ff449435b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-18 18:36:51 +0300

    Refactor profile info container to features component

---
+++ client/src/features/profile/components/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,75 @@
+export default function ProfileInfo() {
+  return (
+    <>
+      <div className="w-96 h-max bg-white rounded-lg shadow">
+        {/* Profile Image */}
+        <div className="flex justify-center mt-4">
+          <div className="w-32 h-32 flex overflow-hidden rounded-full">
+            <img
+              src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1725062400&semt=ais_hybrid"
+              className="object-cover"
+              alt=""
+            />
+          </div>
+        </div>
+        <form className="w-full flex flex-col items-center px-4 py-4">
+          {/* Username */}
+          <div className="mt-4 w-full">
+            <label htmlFor="username" className="block text-sm font-bold">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              required
+              placeholder="Enter your username..."
+              className="py-2 px-4 w-full outline-none rounded-lg border mt-1 transition duration-300 focus:ring-2 focus:ring-primaryBlue"
+            />
+          </div>
+          {/* Email */}
+          <div className="mt-4 w-full">
+            <label htmlFor="email" className="block text-sm font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              required
+              placeholder="Enter your email..."
+              className="py-2 px-4 w-full outline-none rounded-lg border mt-1 transition duration-300 focus:ring-2 focus:ring-primaryBlue"
+            />
+          </div>
+          {/* First Name */}
+          <div className="mt-4 w-full">
+            <label htmlFor="firstName" className="block text-sm font-bold">
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              required
+              placeholder="e.g John"
+              className="py-2 px-4 w-full outline-none rounded-lg border mt-1 transition duration-300 focus:ring-2 focus:ring-primaryBlue"
+            />
+          </div>
+          {/* Last Name */}
+          <div className="mt-4 w-full">
+            <label htmlFor="lastName" className="block text-sm font-bold">
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              required
+              placeholder="e.g Doe"
+              className="py-2 px-4 w-full outline-none rounded-lg border mt-1 transition duration-300 focus:ring-2 focus:ring-primaryBlue"
+            />
+          </div>
+          <button className="mt-4 w-full py-2 flex justify-center text-sm bg-primaryBlue text-white rounded-lg shadow transition duration-300 hover:ring-1 hover:ring-primaryBlue">
+            Save
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,9 +1,19 @@
-export default function Profile() {
-    return (
-        <>
-            <main className="w-full min-h-full absolute overflow-x-hidden bg-mainBackground font-montserrat">
+import ProfileInfo from "../../features/profile/components/ProfileInfo/ProfileInfo";
+import TopBar from "../../shared/components/TopBar/TopBar";
 
-            </main>
-        </>
-    )
+export default function Profile() {
+  return (
+    <>
+      <section className="grow max-w-full min-h-full overflow-x-hidden pb-4">
+        <TopBar pageName={"Profile"} />
+        <div className="mt-8 flex flex-wrap px-4 gap-x-4 gap-y-4">
+          <div className="grow flex justify-center">
+            {/* Profile Info */}
+            <ProfileInfo />
+          </div>
+          <div className="grow"></div>
+        </div>
+      </section>
+    </>
+  );
 }
